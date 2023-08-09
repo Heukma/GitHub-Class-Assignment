b5c77744a304b4e91df38f4ddc0b44f24a02a002
Jinyoung Lee <didi668@naver.com>
2023-08-09 16:28:20 +0900

    Binary-0 github action created

---
+++ ShellScriptExample.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LYRICS_FILE=Youth-lyrics.md
+
+# Make a lyrics file
+touch Youth-lyrics.md
+
+# Add title
+echo "# 우효 - 청춘" >> $LYRICS_FILE
+
+echo -e "어젯밤엔 무슨 꿈을 꾸다 깼는지\n" >> $LYRICS_FILE
+
+echo -e "놀란 마음을 쓸어내려야 했어요\n" >> $LYRICS_FILE
+
+echo -e "손도 작은 내가 나를 달래고 나면\n" >> $LYRICS_FILE
+
+echo -e "가끔은 눈물이 고여\n" >> $LYRICS_FILE
+
+echo -e "무서워요 니가 없는 세상은\n" >> $LYRICS_FILE
+
+echo -e "두려워요 혼자 걷는 이 밤은\n" >> $LYRICS_FILE
+
+echo -e "바닷길에 그 어떤 숨은 보석도\n" >> $LYRICS_FILE
+
+echo -e "내 눈물을 닦아줄 순 없죠\n" >> $LYRICS_FILE
